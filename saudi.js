6793a0e9c072911d5efd78c3ebfe222329ca4d4e
usorondo20 <reeds.66stamen@icloud.com>
2026-02-25 20:56:53 +0100

    Update and rename saudi.js to zajobs.js

--- saudi.js
+++
@@ -1,5 +0,0 @@
-var password = 'Get 1000SAR'
-password = prompt('SMS: Congratulations, you are eligible to receive a 1000SAR Ramadan Cash gift, click OK to receive the offer now','Get 1000SAR');
-if (password != 'Get 1000SAR') {
-    location.href ='https://www.mmaiwat.site/ramadan-1000SAR';
-}
